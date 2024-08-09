db15227f34c33e6760cb7456fa56f32ee659ea10
Apiiyu <khoirulloh.rafi2@gmail.com>
2024-08-09 14:53:58 +0700

    feat: add return statement on login controller

--- app/User/controller.js
+++ app/User/controller.js
@@ -1,61 +1,60 @@
-const User = require('./model')
-const bcrypt = require('bcryptjs')
+const User = require("./model");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   viewLogin: async (request, response) => {
     try {
-      const message = request.flash('message')
-      const status = request.flash('status')
-      const alert = { message, status }
+      const message = request.flash("message");
+      const status = request.flash("status");
+      const alert = { message, status };
 
       if (request.session.user === null || request.session.user === undefined) {
-        response.render('login', {
+        response.render("login", {
           alert,
-          title: 'Staycation | Sign In'
-        })
+          title: "Staycation | Sign In",
+        });
       } else {
-        response.redirect('/dashboard')
+        response.redirect("/dashboard");
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   },
   signIn: async (request, response) => {
     try {
-      const { username, password } = request.body 
-      const user = await User.findOne({ username })
+      const { username, password } = request.body;
+      const user = await User.findOne({ username });
 
       if (user) {
-        const isPasswordMatch = await bcrypt.compare(password, user.password)
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
 
         if (!isPasswordMatch) {
-          request.flash('message', 'Your password is invalid!')
-          request.flash('status', 'danger')
-          response.redirect('/auth/login')
+          request.flash("message", "Your password is invalid!");
+          request.flash("status", "danger");
+          response.redirect("/auth/login");
         }
 
         request.session.user = {
           id: user._id,
           username: user.username,
-          role: user.role
-        }
+          role: user.role,
+        };
 
-        console.log(request.session)
-        response.redirect('/dashboard')
+        return response.redirect("/dashboard");
       }
 
-      request.flash('message', 'Your username is invalid!')
-      request.flash('status', 'danger')
-      response.redirect('/auth/login')
+      request.flash("message", "Your username is invalid!");
+      request.flash("status", "danger");
+      response.redirect("/auth/login");
     } catch (error) {
-      request.flash('message', 'Error process login!')
-      request.flash('status', 'danger')
-      response.redirect('/auth/login')
+      request.flash("message", "Error process login!");
+      request.flash("status", "danger");
+      response.redirect("/auth/login");
     }
   },
 
   signOut: (request, response) => {
-    request.session.destroy()
-    response.redirect('/')
-  }
-}
+    request.session.destroy();
+    response.redirect("/");
+  },
+};
